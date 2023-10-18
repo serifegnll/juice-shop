a34827fb49d2eb9735f6054e4fb89e1f37dd9cdc
seri <gonulluserife7@gmail.com>
2023-10-18 16:43:45 +0300

    vulnerability fix

--- routes/login.ts
+++ routes/login.ts
@@ -31,10 +31,21 @@ module.exports = function login () {
       })
   }
 
-  return (req: Request, res: Response, next: NextFunction) => {
-    verifyPreLoginChallenges(req) // vuln-code-snippet hide-line
-    models.sequelize.query(`SELECT * FROM Users WHERE email = '${req.body.email || ''}' AND password = '${security.hash(req.body.password || '')}' AND deletedAt IS NULL`, { model: UserModel, plain: true }) // vuln-code-snippet vuln-line loginAdminChallenge loginBenderChallenge loginJimChallenge
-      .then((authenticatedUser: { data: User }) => { // vuln-code-snippet neutral-line loginAdminChallenge loginBenderChallenge loginJimChallenge
+  const { Op } = require('sequelize');
+
+return (req: Request, res: Response, next: NextFunction) => {
+  verifyPreLoginChallenges(req); // vuln-code-snippet hide-line
+  models.User.findOne({
+    where: {
+      email: {
+        [Op.eq]: req.body.email || '',
+      },
+      password: {
+        [Op.eq]: security.hash(req.body.password || ''),
+      },
+      deletedAt: null,
+    },
+  }).then((authenticatedUser: { data: User }) => { // vuln-code-snippet neutral-line loginAdminChallenge loginBenderChallenge loginJimChallenge
         const user = utils.queryResultToJson(authenticatedUser)
         if (user.data?.id && user.data.totpSecret !== '') {
           res.status(401).json({
